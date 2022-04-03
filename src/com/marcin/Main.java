033691679c1ed6047dce40cd45f0d064a3541bb1
Marcin <klasicki.m@gmail.com>
2022-04-03 13:18:15 +0200

    Added gitignore file and package for user interface

--- src/com/marcin/Main.java
+++ src/com/marcin/Main.java
@@ -2,10 +2,13 @@ package com.marcin;
 
 public class Main {
 
+    public static String[][] grid = new String[3][3];
+
     public static void main(String[] args) {
 
         Game game = new Game();
         game.run();
 
     }
+
 }
