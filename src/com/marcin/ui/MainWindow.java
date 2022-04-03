edc42684996b58fc4618b862eb494d75caebc075
Marcin <klasicki.m@gmail.com>
2022-04-03 14:13:34 +0200

    Added labels for players names

--- src/com/marcin/ui/MainWindow.java
+++ src/com/marcin/ui/MainWindow.java
@@ -7,12 +7,18 @@ public class MainWindow extends JFrame {
     JMenu optionsMenu;
     JMenuItem resetGame;
     JMenuItem quit;
+    JLabel p1Name;
+    JLabel p2Name;
+
+
 
     public void init() {
+
 
         setTitle("Kółko i krzyżyk");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setBounds(300, 300, 400, 400);
+        setLayout(null);
 
         optionsMenu = new JMenu("Opcje");
         mainMenuBar = new JMenuBar();
@@ -21,8 +27,18 @@ public class MainWindow extends JFrame {
         optionsMenu.add(resetGame);
         optionsMenu.add(quit);
         mainMenuBar.add(optionsMenu);
-
         setJMenuBar(mainMenuBar);
+
+        p1Name = new JLabel("Gracz 1: ");
+        p2Name = new JLabel("Gracz 2: ");
+
+        p1Name.setBounds(10, 10, 100, 20);
+        p2Name.setBounds(10, 40, 100, 20);
+
+
+        add(p1Name);
+        add(p2Name);
+
 
 
         setVisible(true);
