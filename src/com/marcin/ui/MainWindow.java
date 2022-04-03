d470281fb6aadab0b585f742b91719d4ad1c1c4f
Marcin <klasicki.m@gmail.com>
2022-04-03 15:17:52 +0200

    Added panel with buttons

--- src/com/marcin/ui/MainWindow.java
+++ src/com/marcin/ui/MainWindow.java
@@ -1,5 +1,6 @@
 package com.marcin.ui;
 
+import java.awt.*;
 import javax.swing.*;
 
 public class MainWindow extends JFrame {
@@ -9,7 +10,11 @@ public class MainWindow extends JFrame {
     JMenuItem quit;
     JLabel p1Name;
     JLabel p2Name;
+    JLabel p1NameText;
+    JLabel p2NameText;
 
+    private final int buttonHeight = 100;
+    private final int buttonWidth = 100;
 
 
     public void init() {
@@ -17,7 +22,7 @@ public class MainWindow extends JFrame {
 
         setTitle("Kółko i krzyżyk");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setBounds(300, 300, 400, 400);
+        setBounds(300, 300, 316, 462);
         setLayout(null);
 
         optionsMenu = new JMenu("Opcje");
@@ -31,23 +36,59 @@ public class MainWindow extends JFrame {
 
         p1Name = new JLabel("Gracz 1: ");
         p2Name = new JLabel("Gracz 2: ");
+        p1NameText = new JLabel("Marcin");
+        p2NameText = new JLabel("Marcin");
 
-        p1Name.setBounds(10, 10, 100, 20);
-        p2Name.setBounds(10, 40, 100, 20);
-
+        p1Name.setBounds(10, 10, 50, 20);
+        p2Name.setBounds(10, 40, 50, 20);
+        p1NameText.setBounds(70, 10, 150, 20);
+        p2NameText.setBounds(70, 40, 150, 20);
 
         add(p1Name);
         add(p2Name);
+        add(p1NameText);
+        add(p2NameText);
 
-
+        addButtonsPanel(this);
 
         setVisible(true);
     }
 
 
+    private void addButtonsPanel(JFrame frame){
+        JPanel buttonsPanel = new JPanel();
+        buttonsPanel.setLayout(null);
+        buttonsPanel.setBounds(0, 100, 300, 300);
 
+        JButton[] buttons = generateButtons();
 
+        buttons[0].setBounds(0, 0, buttonWidth, buttonHeight);
+        buttons[1].setBounds(100, 0, buttonWidth, buttonHeight);
+        buttons[2].setBounds(200, 0, buttonWidth, buttonHeight);
+        buttons[3].setBounds(0, 100, buttonWidth, buttonHeight);
+        buttons[4].setBounds(100, 100, buttonWidth, buttonHeight);
+        buttons[5].setBounds(200, 100, buttonWidth, buttonHeight);
+        buttons[6].setBounds(0, 200, buttonWidth, buttonHeight);
+        buttons[7].setBounds(100, 200, buttonWidth, buttonHeight);
+        buttons[8].setBounds(200, 200, buttonWidth, buttonHeight);
 
+        for(JButton button : buttons) {
+            buttonsPanel.add(button);
+        }
+
+        frame.add(buttonsPanel);
+
+    }
+
+    private JButton[] generateButtons() {
+        JButton[] buttons = new JButton[9];
+
+        for(int i = 0; i< buttons.length; i++) {
+            buttons[i] = new JButton("X");
+        }
+
+        return buttons;
+    }
 
 
 }
